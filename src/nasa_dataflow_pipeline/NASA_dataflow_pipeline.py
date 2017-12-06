225bb90ffe929505414013670ec4ce6f15d4898f
epugmire <edward.pugmire@gmail.com>
2017-12-06 13:51:33 -0500

    Update NASA_dataflow_pipeline.py

--- src/nasa_dataflow_pipeline/NASA_dataflow_pipeline.py
+++ src/nasa_dataflow_pipeline/NASA_dataflow_pipeline.py
@@ -53,7 +53,7 @@ def run(argv=None):
 				lat_long = re.findall(r'\"(.+?)\"',element)
 				if lat_long:
 					response = urllib2.urlopen('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat_long[0] + '&location_type=APPROXIMATE&result_type=country|locality&key=AIzaSyCc_Z2Jqqa1JLhZhSaURtUhF-N34PCQwrw')
-					return [lat_long[0] + ','+element.rsplit(',',1)[1] + ',' + json.dumps(response)]
+					return [lat_long[0] + ','+element.rsplit(',',1)[1] + ',' + json.dumps(response, default=set_default)]
 				##return ['https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat_long[0] + '&location_type=APPROXIMATE&result_type=country|locality&key=AIzaSyCc_Z2Jqqa1JLhZhSaURtUhF-N34PCQwrw']
 
 		##urlData = "http://api.openweathermap.org/data/2.5/weather?q=Boras,SE"
